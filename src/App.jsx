d08b1465afe371aae57530aa701e7c4980de1cb5
Vixs101 <elijahvix695@gmail.com>
2024-01-23 12:48:56 +0100

    made a few changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,25 +12,27 @@ function App() {
         <button className="border-2 p-3 rounded-xl bg-black text-white text-lg"> Connect Wallet</button>
       </nav>
 
-      <div className=" flex w-full h-screen mt-auto justify-center bg-gray-50">
-        <div className="w-1/3 h-1/3 border-2 border-black py-3 px-3 bg-gray-200" >
-          <form action="submit" >
-            <input type="text" id="address" name="walletAddress"/>
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              fill="none" 
-              viewBox="0 0 24 24" 
-              strokeWidth={1.5} 
-              stroke="currentColor" 
-              className="w-6 h-6">
-              <path 
-                strokeLinecap="round" 
-                strokeLinejoin="round" 
-                d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
-            </svg>
+      <div className=" flex w-full h-96 items-center justify-center bg-gray-50">
+        <div className=" w-1/3 h-1/3 border-2 border-black py-3 px-3 bg-gray-200" >
+          <form action="flex flex-col gap-5 submit" >
+            {/* receivers wallet */}
+            <div className="flex justify-between bg-gray-50 items-center rounded-lg px-2">
+              <input type="text" id="address" name="walletAddress" placeholder="Wallet Address" className="p-2 bg-transparent w-10/12"/>
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                strokeWidth={1.5} 
+                stroke="currentColor" 
+                className="w-6 h-6 ">
+                <path 
+                  strokeLinecap="round" 
+                  strokeLinejoin="round" 
+                  d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
+              </svg>
+            </div>
 
-            <label for="amount">Amount:</label>
-            <input type="amount" id="amount" name="amount"></input>
+            <input type="amount" id="amount" name="amount" placeholder="Amount" className="p-2 bg-transparent w-10/12"></input>
           </form>
         </div>
       </div>
