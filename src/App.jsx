7857724e67b024bfe823759c0c82bd9397066c3c
Vixs101 <elijahvix695@gmail.com>
2024-01-23 11:54:19 +0100

    started working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className='flex justify-between items-center py-3 px-4 bg-gray-200'>
+        <div>
+          <h1 className='name text-4xl'> Elijah Victor</h1>
+        </div>
+        <button className="border-2 p-3 rounded-xl bg-black text-white text-lg"> Connect Wallet</button>
+      </nav>
     </>
   )
 }
