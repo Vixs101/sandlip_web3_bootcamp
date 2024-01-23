fb4dfb691fcfb4000f12dc5885d01c3a62b9a143
Vixs101 <elijahvix695@gmail.com>
2024-01-23 12:21:37 +0100

    created the forms for the wallet

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,27 @@ function App() {
         <button className="border-2 p-3 rounded-xl bg-black text-white text-lg"> Connect Wallet</button>
       </nav>
 
-      <div>
-        fi
+      <div className=" flex w-full h-screen items-center justify-center bg-gray-50">
+        <div className="border-2 border-black w-1/3 py-3 px-3" >
+          <form action="submit" >
+            <input type="text" id="address" name="walletAddress"/>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" 
+              viewBox="0 0 24 24" 
+              strokeWidth={1.5} 
+              stroke="currentColor" 
+              className="w-6 h-6">
+              <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
+            </svg>
+
+            <label for="amount">Amount:</label>
+            <input type="amount" id="amount" name="amount"></input>
+          </form>
+        </div>
       </div>
     </>
   )
